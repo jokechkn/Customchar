04079ed5ce952bce3e083d9e61fd6b3e8d41414b
jokechkn <jokechkn@gmail.com>
2025-08-24 18:59:18 -0300

    Create Perm.lua

---
+++ Perm.lua
@@ -0,0 +1,26 @@
+local Tabela = {
+	PlayerPermission = {
+		["Kayky97315"] = 			{Tier = 2000},
+		["robobitinho"] = 			{Tier = 2000},
+		["rip_rimuro283"] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {}},
+		["DIO"]	=					{Locked = {3510823553}},
+		["Madara"]	=				{Locked = {3510823553,2019379740}},
+		["Koku"]	=				{Locked = {3510823553,2019379740}},
+		["Yori"]	=				{Permission = {}},
+		["Meliodas"]	=				{Permission = {}},
+		["Goku"]	=				{Locked = {3510823553}},
+		["TOP"]	=					{Locked = {3510823553}},
+		["Giorno"] = 					{Tier = 1050,Permission = {}},
+		["Hakaishin"] = 				{Locked = {3510823553}},
+		["Boss"] = 					{Locked = {}},
+		["JP6"] = 					{Tier = 1,Permission = {}},
+		["Gojo"] = 					{Tier = 1,Permission = {}}, -- perm temp
+	}
+
+}
+
+return Tabela
